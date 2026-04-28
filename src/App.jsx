0baba538c4174c185e8b01219b6ffbdf0c630509
ms21alsaadi-wq <ms21alsaadi@gmail.com>
2026-04-28 12:45:59 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import "./App.css";
 
 function App() {
   return (
     <div>
 
-      {/* HEADER */}
       <div className="header">
-
         <div className="logo">LUXE SOLE</div>
 
         <div className="nav-links">
@@ -16,25 +13,21 @@ function App() {
         </div>
 
         <div className="actions">
-
           <div className="icon-btn">👤</div>
 
           <div className="cart">
             <span className="icon-btn">🛒</span>
             <span className="cart-count">3</span>
           </div>
-
         </div>
-
       </div>
 
-      {/* محتوى مؤقت */}
       <div style={{ padding: "40px", textAlign: "center" }}>
         المتجر شغال 🔥
       </div>
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
